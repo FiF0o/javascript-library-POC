9d4a44d263c6036bcc2a6fa4a3a0d98824ab1cf2
jon <jonathan.lazarini@wipro.com>
2017-02-03 23:25:30 +0000

    add unit test

---
+++ src/index.test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jonlazarini on 03/02/17.
+ */
+var expect = require('chai').expect;
+var starW = require('./index');
+
+// global describe from mocha
+describe('javascript-library', function() {
+
+    it('should work!', function() {
+       expect(true).to.be.true;
+    });
+
+});
