700212e002623f00d106fe687ee89663c603040d
jon <jonathan.lazarini@wipro.com>
2017-02-04 00:30:38 +0000

    sign up to travis ci and add semantic-release pkg to version when deploying

--- src/index.test.js
+++ src/index.test.js
@@ -7,8 +7,32 @@ var starW = require('./index');
 // global describe from mocha
 describe('javascript-library', function() {
 
-    it('should work!', function() {
-       expect(true).to.be.true;
+    describe('all', function() {
+
+        it('should be an array of strings', function() {
+            expect(starW.all).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                });
+            }
+        });
+
+        it('should contain `Beta test`', function() {
+           expect(starW.all).to.include('Beta test');
+        });
+
+    });
+
+
+    describe('random', function() {
+
+        it('should return a random item from starW.all', function() {
+            var randItem = starW.random();
+            expect(starW.all).to.include(randItem);
+        });
+
     });
 
 });
